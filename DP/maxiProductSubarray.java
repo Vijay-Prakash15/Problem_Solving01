5900d577b024cd6f86ab905d10bae9dd9a27d090
Vijay Prakash <vijaypr82701@gmail.com>
2026-02-08 21:08:03 +0530

    another approach

--- DP/maxiProductSubarray.java
+++ DP/maxiProductSubarray.java
@@ -13,8 +13,24 @@ public class maxiProductSubarray {
         }
         return max;
     }
+    public static int maxProduct(int[] arr) {
+        // code here
+        int prd = Integer.MIN_VALUE;
+        int pre = 1, suf = 1;
+        int n = arr.length;
+        
+        for(int i=0; i<n; i++){
+            if(pre == 0) pre = 1;
+            if(suf == 0) suf = 1;
+            
+            pre *= arr[i];
+            suf *= arr[n-1-i];
+            prd = Math.max(prd, Math.max(pre, suf));
+        }
+        return prd;
+    }
     public static void main(String[] args) {
-        int[] arr = {-2, 6, -3, -10, 0, 2};
-        System.err.println("Maximum Product Subarray:- "+ maximumProduct(arr));
+        int[] arr = {-2, 6, -3, -10, 10, 2};
+        System.err.println("Maximum Product Subarray:- "+ maxProduct(arr));
     }
 }
